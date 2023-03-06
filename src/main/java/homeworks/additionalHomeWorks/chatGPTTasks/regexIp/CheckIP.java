77e2e7ac5e665618e532acead63beddf71c4c74a
luxorious <vovan_194@mail.ru>
2023-03-06 18:03:58 +0100

    regEx

--- src/main/java/homeworks/additionalHomeWorks/chatGPTTasks/regexIp/CheckIP.java
+++ src/main/java/homeworks/additionalHomeWorks/chatGPTTasks/regexIp/CheckIP.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class CheckIP {
     public boolean isValid(String ip){
-        String regex = "\\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\b";
+        String regex = "\\b((25[0-5]|2[0-4]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4]|[01]?[0-9][0-9]?)\\b";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(ip);
         if (matcher.find()){
